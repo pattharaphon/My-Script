ecc7b792a9f6c910e4bbfc44958a0edb33535f91
pattharaphon <52312568+pattharaphon@users.noreply.github.com>
2019-09-13 17:08:17 +0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+#Initialize a variable withauser-specified value. 
+user = input("I	am Python. What	is your	name? :")
+#Output	astring	and a variable value. 
+print("Welcome",user) 
+#Initialize another variable with a user-specified value. 
+lang = input("Favorite programming language? :")
+#Output	a string and a variable value. 
+print(lang,"Is","Fun",sep ="*",end="!\n") 
---
+++ syntax.py
@@ -0,0 +1,2 @@
+print("Python ineasy steps)
+title ="Python in easy steps" print(titel)	
